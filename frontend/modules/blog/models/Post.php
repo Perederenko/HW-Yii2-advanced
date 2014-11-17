55fd15fb2871bf1ddd7a601a513e96ecdf4d9301
Perederenko <marina2206@mail.ru>
2014-11-17 15:05:35 +0200

    Created model post

---
+++ frontend/modules/blog/models/Post.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\modules\blog\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%post}}".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $text
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%post}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'text', 'created_at', 'updated_at'], 'required'],
+            [['created_at', 'updated_at'], 'integer'],
+            [['title', 'text'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'text' => 'Text',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
