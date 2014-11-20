2cc6754dc2b8e718e2f548ec5c1ccfaf33b4a196
Perederenko <marina2206@mail.ru>
2014-11-20 18:07:54 +0200

    Created migration: product

---
+++ console/migrations/m141120_122936_product.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141120_122936_product extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%product}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+            'description' => Schema::TYPE_TEXT,
+            'price' => Schema::TYPE_DECIMAL . '(10, 2) NOT NULL',
+            'image' => Schema::TYPE_STRING,
+            'category_id' => Schema::TYPE_INTEGER . '(10) NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%product}}');
+    }
+}
