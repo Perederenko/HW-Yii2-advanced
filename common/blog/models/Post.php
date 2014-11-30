482545a5bc6bc8a089a8f69136e3253cccddca6c
Perederenko <marina2206@mail.ru>
2014-11-30 14:58:38 +0200

    created post model

---
+++ common/blog/models/Post.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace common\blog\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%post}}".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $anons
+ * @property string $content
+ * @property string $publish_date
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%post}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'anons', 'content'], 'required'],
+            [['anons', 'content'], 'string'],
+            [['publish_date'], 'safe'],
+            [['title'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'anons' => 'Anons',
+            'content' => 'Content',
+            'publish_date' => 'Publish Date',
+        ];
+    }
+}
